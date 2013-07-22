f70a7b79e33194f22ca6814f6a296f55e7fd5088
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2013-07-22 15:48:57 -0400

    Getting somewhere.  Have tests actually failing.

--- test/index.test.js
+++ test/index.test.js
@@ -43,7 +43,20 @@ describe('forro', function(){
             'username': forro.string().required(),
             'password': forro.string().required()
         });
-        assert.throws(new AuthForm({'username': 'a'}).validate, Error);
+        assert.throws(function(){
+            new AuthForm({'username': 'a'}).validate();
+        }, Error);
+    });
+
+    it('should throw if a required field is just whitespace', function(){
+        var AuthForm = forro({
+            'username': forro.string().required(),
+            'password': forro.string().required()
+        });
+
+        assert.throws(function(){
+            new AuthForm({'username': '        ', 'password': 'ja'}).validate();
+        }, Error);
     });
 
     it('should allow required and optional fields', function(){
@@ -51,58 +64,9 @@ describe('forro', function(){
             'username': forro.string().required(),
             'password': forro.string().required(),
             'rememberMe': forro.boolean()
-        }), form;
+        });
 
-        form = new AuthForm({'username': 'a', 'password':'b'});
-        assert(form.validate());
+        assert(new AuthForm({'username': 'a', 'password':'b'}).validate());
     });
 
-    // it('allows optional and required fields', function(){
-    //     var AuthForm = forro({
-    //         'username': forro.string().required(),
-    //         'password': forro.string().required(),
-    //         'rememberMeBro': forro.string()
-    //     }), form;
-
-    // //     form = new AuthForm(new FakeRequest({'username': 'a', 'password':'b'}),
-    // //         new FakeResponse());
-
-    // // Object.keys(form.fields).forEach(function(name){
-    // //     if(name === 'rememberMeBro'){
-    // //         assert(form.field(name).optional);
-    // //         assert(!form.field(name).required);
-    // //     }
-    // //     else{
-    // //         assert.ok(form.field(name).required);
-    // //     }
-    // // });
-    // });
-    // it('validating valid data works as expected', function(){
-    //     var AuthForm = forro({
-    //             'username': forro.string().required(),
-    //             'password': forro.string().required(),
-    //             'rememberMeBro': forro.string()
-    //         });
-    //         // req = new FakeRequest({'username': 'a', 'password':'b'}),
-    //         // res = new FakeResponse(),
-    //         // form = new AuthForm(req, res);
-
-    //     // form.validate();
-    //     // assert(form.errors.length === 0);
-
-    // });
-    // it('fails validation for a simple missing string', function(){
-    //     var AuthForm = forro({
-    //             'username': forro.string().required(),
-    //             'password': forro.string().required(),
-    //             'rememberMeBro': forro.string()
-    //         });
-    //     //     req = new FakeRequest({'password':'b'}),
-    //     //     res = new FakeResponse(),
-    //     //     form = new AuthForm(req, res);
-
-    //     // form.validate();
-    //     // assert(form.errors.length === 1);
-    //     // assert(form.errors[0].field.name === 'username');
-    // });
 });
